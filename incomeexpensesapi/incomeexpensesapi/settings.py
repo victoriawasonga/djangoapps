6d54936ecac020e5cdc6ab073b156f0daeb52e00
Victoria Wasonga <victoria.wasonga@strathmore.edu>
2023-07-12 12:58:16 +0300

    more on registration

--- incomeexpensesapi/incomeexpensesapi/settings.py
+++ incomeexpensesapi/incomeexpensesapi/settings.py
@@ -83,7 +83,9 @@ DATABASES = {
     }
 }
 
-
+REST_FRAMEWORK={
+    'NON_FIELD_ERRORS_KEY':'error'
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
