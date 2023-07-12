eca31bb9e318ea8c2a12096ce86b5c01ae356c48
Victoria Wasonga <victoria.wasonga@strathmore.edu>
2023-07-12 10:55:32 +0300

    custom usetr model

--- incomeexpensesapi/incomeexpensesapi/settings.py
+++ incomeexpensesapi/incomeexpensesapi/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODELS='authentication.User'
 
 # Application definition
 
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
